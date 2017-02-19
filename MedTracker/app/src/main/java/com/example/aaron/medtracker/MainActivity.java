b36c947c18d7f8e126b4994f4357f8ab6bdc02a5
Aaron <marksaaron@brandeis.edu>
2017-02-19 00:02:00 -0500

    testing repository via Android Studio 2

--- MedTracker/app/src/main/java/com/example/aaron/medtracker/MainActivity.java
+++ MedTracker/app/src/main/java/com/example/aaron/medtracker/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.aaron.medtracker;
 
+import android.app.AlarmManager;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,8 +10,18 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.TimePicker;
+
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
+
+    AlarmManager alarm_manager;
+    TimePicker pickTime;
+    TextView update_text;
+    Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,15 +29,30 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-       FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        this.context =this;
+        pickTime =(TimePicker)findViewById(R.id.timePicker);
+        alarm_manager =(AlarmManager)getSystemService(ALARM_SERVICE);
+        update_text=(TextView)findViewById(R.id.updateAlarm);
+        Calendar calendar = Calendar.getInstance();
+        Button StartAlarm = (Button)findViewById(R.id.alarm_on);
+        StartAlarm.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v) {
+                set_alarm_text("Alarm on");
             }
         });
+        Button EndAlarm = (Button)findViewById(R.id.alarm_off);
+        EndAlarm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                set_alarm_text("Alarm off");
+            }
+        });
+
+    }
+
+    private void set_alarm_text(String output) {
+        update_text.setText(output);
     }
 
     @Override
